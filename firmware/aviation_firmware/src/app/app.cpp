6059661ccf0a4881e306a5e478de879b76dcb677
Adrian Winter <67652600+Adrianw27@users.noreply.github.com>
2025-12-10 05:00:52 -0500

    Update app.cpp

--- firmware/aviation_firmware/src/app/app.cpp
+++ firmware/aviation_firmware/src/app/app.cpp
@@ -6,11 +6,13 @@ namespace app {
 void app_init() {
 	app::set_mode(app::Mode::Init);
 	scheduler_init();
+	return;
 }
 
 void app_loop_tick() {
 	app::set_mode(app::Mode::Active);
 	scheduler_tick();
+	return;
 }
 
 }
