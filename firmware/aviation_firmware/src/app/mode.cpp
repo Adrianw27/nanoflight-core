6bf27349b5b1d5314431dd5947bb6684e44ec004
Adrian Winter <67652600+Adrianw27@users.noreply.github.com>
2025-12-10 05:00:34 -0500

    Refactor set_mode and get_mode functions

--- firmware/aviation_firmware/src/app/mode.cpp
+++ firmware/aviation_firmware/src/app/mode.cpp
@@ -4,13 +4,12 @@ namespace app {
 
 static Mode g_mode = Mode::Boot;
 
-void set_mode(Mode mode)
-{
+void set_mode(Mode mode) {
     g_mode = mode;
+    return;
 }
 
-Mode get_mode()
-{
+Mode get_mode() {
     return g_mode;
 }
 
